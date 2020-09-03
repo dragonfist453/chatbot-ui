c894258771dadeed890ed0af0641c54aafc0d562
dragonfist453 <thermosphere453@gmail.com>
2020-09-03 15:53:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App() {
+    <div>
+        <p>Hello World!</p>
+    </div>
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const chatApp = document.getElementById('chat-app');
+ReactDOM.render(
+    <React.Frament>
+        <App/>
+    </React.Frament>,
+    chatApp
+);
