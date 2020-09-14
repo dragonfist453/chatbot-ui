82214221fbe080c6effc8a96cd96aed078606b56
Ambu Karthik <thermosphere453@gmail.com>
2020-09-15 00:08:07 +0530

    Added prompt for input server URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./style.css";
 import axios from 'axios';
 
 export default function App() {
+    const url = prompt("Please enter the URL where the server is running", "http://localhost:5000")
     // Print hello in the beginning of the app
     React.useEffect(() => {
         addResponseMessage(`Hello!`)
@@ -17,7 +18,7 @@ export default function App() {
 
     // Gets the response from python server using axios
     const getBotResponse = async (userMessage) => {
-        await axios.post('http://localhost:5000/', `${userMessage}`)
+        await axios.post(url, `${userMessage}`)
             .then(res => {
                 res.data.messages.map(message => {
                     addResponseMessage(`${message}`)
