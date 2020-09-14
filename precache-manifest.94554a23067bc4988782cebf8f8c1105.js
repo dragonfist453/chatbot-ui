8f5d4a11a380f9088c18a531bcba89c34d73b69e
Ambu Karthik <thermosphere453@gmail.com>
2020-09-15 00:09:13 +0530

    Updates

---
+++ precache-manifest.94554a23067bc4988782cebf8f8c1105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbc349c92abfc09a7560e73efca76d7",
+    "url": "/chatbot-ui/index.html"
+  },
+  {
+    "revision": "7757f04f6dcdecf2138c",
+    "url": "/chatbot-ui/static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "a8a14d2d2dffb92f34e0",
+    "url": "/chatbot-ui/static/css/main.aab134c0.chunk.css"
+  },
+  {
+    "revision": "7757f04f6dcdecf2138c",
+    "url": "/chatbot-ui/static/js/2.745c286b.chunk.js"
+  },
+  {
+    "revision": "7a0c0758d9bfdc25a40edd58cae2b6db",
+    "url": "/chatbot-ui/static/js/2.745c286b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a14d2d2dffb92f34e0",
+    "url": "/chatbot-ui/static/js/main.107532c5.chunk.js"
+  },
+  {
+    "revision": "b6565cb61b19fa9fd994",
+    "url": "/chatbot-ui/static/js/runtime-main.9e0f6e60.js"
+  }
+]);
